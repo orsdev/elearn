04c2b663e65f83a19f64dd94bcd0188584c08aed
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-06 16:41:05 +0100

    packages installed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+ReactDOM.render( <App />,
   document.getElementById('root')
 );
 
