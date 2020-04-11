003e3cb7cf9afe272c6a7ac7044079d4ce905e46
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-11 20:50:49 +0100

    storeFactory function added to test utils

--- src/test/utils.js
+++ src/test/utils.js
@@ -1,4 +1,11 @@
+import ReduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import combinedReducer from '../store/reducers/combinedReducer.js';
 
 export const findByAttr = (wrapper, value) => {
  return wrapper.find(`[data-test='${value}']`);
+};
+
+export const storeFactory = (initialState) => {
+ return createStore(combinedReducer, initialState, applyMiddleware(ReduxThunk));
 };
