a7d7c40e7a7003a53bd28a7f05b5245b8a992a12
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-11 17:30:32 +0100

    authenticate reducer test all passed

---
+++ src/store/reducers/authentication/authenticate.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Authenticate from './authenticate';
+import { types } from '../../types';
+
+describe('Authenticate reducer', () => {
+
+ test('Should return default state', () => {
+  const authenticate = Authenticate(null, {});
+  expect(authenticate).toEqual(null);
+ });
+
+ test('Should return new state if type is provided', () => {
+
+  const user = {
+   user: {
+    googleId: "",
+    imageUrl: "",
+    email: "",
+    name: "",
+    givenName: "",
+    familyName: ""
+   },
+   auth: true
+  }
+
+  const newState = {
+   type: types.USER,
+   user: user.user
+  }
+
+  const authenticate = Authenticate(user, newState);
+  expect(authenticate).toEqual(user);
+ });
+
+});
