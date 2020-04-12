35f17fcdb952a3caa5dbe2b22ff7648a8421404b
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-12 21:09:56 +0100

    more test written and passed for authenticate reducer

--- src/store/reducers/authentication/authenticate.test.js
+++ src/store/reducers/authentication/authenticate.test.js
@@ -9,7 +9,7 @@ describe('Authenticate reducer', () => {
   expect(authenticate).toEqual(null);
  });
 
- test('Should return new state if type is provided', () => {
+ test('Should return new state if action.type(LOGIN_USER) is provided', () => {
 
   const user = {
    user: {
@@ -26,6 +26,21 @@ describe('Authenticate reducer', () => {
   const newState = {
    type: types.LOGIN_USER,
    user: user.user
+  }
+
+  const authenticate = Authenticate(user, newState);
+  expect(authenticate).toEqual(user);
+ });
+
+ test('Should return new state if action.type(LOGOUT_USER) is provided', () => {
+
+  const user = {
+   user: null,
+   auth: false
+  }
+
+  const newState = {
+   type: types.LOGOUT_USER
   }
 
   const authenticate = Authenticate(user, newState);
