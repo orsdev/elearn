c8c2b0ee69092a919b444301168dbb94dd347be7
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-15 09:54:01 +0100

    loginButton.js compoenent refactor, loginButton.js test passes

--- src/component/Button/loginButton/logInButton.js
+++ src/component/Button/loginButton/logInButton.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import * as action from '../../../store/action';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import GoogleLogin from 'react-google-login';
 
@@ -10,20 +8,15 @@ const LoginButton = (props) => {
 
  }, [props.auth]);
 
- const responseGoogle = (response) => {
-  if (response) {
-   props.onLogInUser(response.profileObj);
-  }
- };
-
  if (!props.auth) {
   var button = (
    <GoogleLogin
+    data-test="google-login-button"
     className="login-btn"
     clientId={process.env.REACT_APP_CLIENT_ID}
     buttonText="Log in with Google"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
+    onSuccess={props.responseGoogle}
+    onFailure={props.responseGoogle}
     isSignedIn={true}
     cookiePolicy={"single_host_origin"} />
   )
@@ -38,20 +31,8 @@ const LoginButton = (props) => {
 };
 
 LoginButton.propTypes = {
- onLogInUser: PropTypes.func.isRequired
+ responseGoogle: PropTypes.func,
+ auth: PropTypes.bool
 };
 
-const mapStateToProps = (state) => {
- return {
-  auth: state.authenticate.auth,
-  user: state.authenticate.user
- }
-};
-
-const mapDispatchToProps = (dispatch) => {
- return {
-  onLogInUser: (user) => dispatch(action.logInUser(user))
- }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginButton);
+export default LoginButton;
--- src/component/Button/loginButton/logInButton.test.js
+++ src/component/Button/loginButton/logInButton.test.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import LoginButton from './logInButton';
 import { shallow } from 'enzyme';
-import { findByAttr, storeFactory, checkProps } from '../../../test/utils';
+import { findByAttr, checkProps } from '../../../test/utils';
 
 
-const setUp = (initialState) => {
- const store = storeFactory(initialState);
- const wrapper = shallow(<LoginButton store={store} />).dive().dive();
+const setUp = (props = {}) => {
+ const wrapper = shallow(<LoginButton {...props} />);
  return wrapper;
 };
 
 describe('loginButton Component', () => {
 
- let wrapper;
-
- beforeEach(() => {
-  wrapper = setUp();
- });
-
  test('Should render without error', () => {
+  let wrapper = setUp(undefined);
   const component = findByAttr(wrapper, 'loginButton-component');
   expect(component.length).toBe(1);
  });
 
+ test('Should render Google login button', () => {
+  let wrapper = setUp({ auth: false });
+  const component = findByAttr(wrapper, 'google-login-button');
+  expect(component.length).toBe(1);
+ });
+
  test('does not throw warning with expected props', () => {
-  const expectedProps = { onLogInUser: () => { } };
+  const expectedProps = {
+   responseGoogle: () => { },
+   auth: false
+  };
+
   checkProps(LoginButton, expectedProps);
  });
 
