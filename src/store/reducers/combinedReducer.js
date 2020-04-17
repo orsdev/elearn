58ff93f9a5a15e0467f10f6c0f803536e5dd9f6b
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-17 09:39:41 +0100

    playListItems reducer added to combineReducers

--- src/store/reducers/combinedReducer.js
+++ src/store/reducers/combinedReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authenticate from './authentication/authenticate';
 import playListId from './playListId/playListId';
+import playListItems from './playListItems/playListItems';
 
 const combinedReducer = combineReducers({
  authenticate,
- playListId
+ playListId,
+ playListItems
 });
 
 export default combinedReducer;
