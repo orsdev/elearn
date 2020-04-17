6ace8644e5aecfa7d09ab6c44c72632ef6c2172e
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-17 20:14:47 +0100

    loginNotification component added,test passes

---
+++ src/component/loginNotification/loginNotification.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const loginNotification = () => {
+ return (
+  <div
+   data-test="loginNotification-component"
+   className="loginNotification">
+   <h1 data-test="login-text"> Login to see available courses!!!</h1>
+  </div>
+ )
+};
+
+export default loginNotification;
