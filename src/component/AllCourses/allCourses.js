86ed8c0e8b6d60e8a46ac3b261a85cd992fb7fe0
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-18 19:24:23 +0100

    allCourses component added to app

---
+++ src/component/AllCourses/allCourses.js
@@ -0,0 +1,80 @@
+import React, { useEffect, Fragment } from 'react';
+import * as action from '../../store/action';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const AllCourses = (props) => {
+
+ const { auth, id } = props;
+ const { onGetPlayListId, onGetPlayListItems, onRemovePlayList } = props;
+
+ useEffect(() => {
+
+  if (auth) {
+   onGetPlayListId();
+  }
+
+  if (id && auth) {
+   onGetPlayListItems(id, null)
+  }
+
+  if (!auth) {
+   onRemovePlayList();
+  }
+
+ }, [auth, id]);
+
+ if (props.playlist !== null) {
+  var playlist = props.playlist.map((item, index) => {
+   console.log(item)
+   let videoId = item.snippet.resourceId.videoId;
+   let title = item.snippet.title;
+   let description = item.snippet.description;
+
+   return (
+    <Fragment key={index + videoId}>
+     <div className="grid-item" id={videoId}>
+      <iframe title={title} src={"https://www.youtube.com/embed/" + videoId} video="true" frameBorder={0} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <div className="courses-body">
+       <h4 className="courses-title">{title}</h4>
+       <p className="courses-about">{description}</p>
+       <div className="fav-icon">
+        <i className="fa fa-heart-o" data-fav="add-fav" id={videoId} aria-hidden="true"></i>
+       </div>
+      </div>
+     </div>
+    </Fragment>
+   )
+  })
+ }
+
+ return (
+  <div className="allCourses">
+   <h2>All Courses </h2>
+   <div className="grid-container">
+    {!auth ? <Redirect to="/" /> : null}
+    {playlist}
+   </div>
+  </div>
+ )
+};
+
+
+const mapStateToProps = (state) => {
+ return {
+  auth: state.authenticate.auth,
+  id: state.playListId,
+  playlist: state.playListItems.playlist
+ }
+};
+
+const mapDispatchToProps = (dispatch) => {
+ return {
+  onGetPlayListId: () => dispatch(action.getPlaylistId()),
+  onGetPlayListItems: (id, max) => dispatch(action.getPlaylistItems(id, max)),
+  onRemovePlayList: () => dispatch(action.removePlaylist()),
+ }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllCourses);
