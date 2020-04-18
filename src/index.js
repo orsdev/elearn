9bf809446d1b2b1ba6ffd6ad5fe12f1be5acafa7
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-18 19:22:59 +0100

    router added to app

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/createStore';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 const app = (
  <Provider store={store}>
-  <App />
+  <Router>
+   <App />
+  </Router>
  </Provider>
 );
 
