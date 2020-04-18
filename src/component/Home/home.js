caf3f729748345ee31f58c5da9cd207e97c8f84d
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-18 11:36:34 +0100

    home component added

---
+++ src/component/Home/home.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from 'react';
+import * as action from '../../store/action';
+import { connect } from 'react-redux';
+import Hero from '../Hero/hero';
+import HomeCourses from '../HomeCourses/homeCourses';
+
+const Home = (props) => {
+
+ //Google auth success function
+ const responseGoogle = (response) => {
+  if (response && !response.error) {
+   props.onLogInUser(response.profileObj, true);
+  }
+ };
+
+ //Google auth logout function
+ const logout = () => {
+  props.onLogOutUser();
+ }
+
+
+ return (
+  <Fragment>
+   <Hero
+    responseGoogle={responseGoogle}
+    auth={props.auth} />
+   <HomeCourses />
+  </Fragment>
+ )
+};
+
+const mapStateToProps = (state) => {
+ return {
+  auth: state.authenticate.auth,
+ }
+};
+
+const mapDispatchToProps = (dispatch) => {
+ return {
+  onLogInUser: (user, auth) => dispatch(action.logInUser(user, auth)),
+  onLogOutUser: () => dispatch(action.logOutUser())
+ }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
