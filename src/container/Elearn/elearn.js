a48d65d3b4b44eb158a0f7d7c00238532dec60fa
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-22 20:13:32 +0100

    elearn component refactored

--- src/container/Elearn/elearn.js
+++ src/container/Elearn/elearn.js
@@ -1,38 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import * as action from '../../store/action';
 import { Route, Switch } from 'react-router-dom';
 import Header from '../../component/Header/header';
 import Home from '../../component/Home/home';
-import AllCourses from '../../component/AllCourses/allCourses';
 import Footer from '../../component/Footer/footer';
 
 class Elearn extends Component {
- constructor(props) {
-  super(props);
-  this.state = {};
- }
-
- //Google auth logout function
- logout = () => {
-  this.props.onLogOutUser();
- }
 
  render() {
 
   let route = (
    <Switch>
-    <Route exact path="/all-courses" component={AllCourses} />
     <Route exact path="/" component={Home} />
    </Switch>
   )
   return (
    <Fragment>
     <Header
-     src={this.props.auth ? this.props.user.imageUrl : null}
-     fullname={this.props.auth ? this.props.user.name : null}
-     logout={this.logout}
-     auth={this.props.auth} />
+     src={this.props.loggedIn ? this.props.userData.imageUrl : null}
+     fullname={this.props.loggedIn ? this.props.userData.name : null}
+     loggedIn={this.props.loggedIn} />
     {route}
     <Footer />
    </Fragment >
@@ -42,16 +29,10 @@ class Elearn extends Component {
 
 const mapStateToProps = (state) => {
  return {
-  auth: state.authenticate.auth,
-  user: state.authenticate.user,
+  loggedIn: state.authenticate.loggedIn,
+  userData: state.authenticate.userData,
   id: state.playListId
  }
 };
 
-const mapDispatchToProps = (dispatch) => {
- return {
-  onLogOutUser: () => dispatch(action.logOutUser())
- }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Elearn);
+export default connect(mapStateToProps, null)(Elearn);
