cedeb7c0e99af77a0bbd202256581dafdbf9034b
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-23 18:13:43 +0100

    elearn component refactored

--- src/container/Elearn/elearn.js
+++ src/container/Elearn/elearn.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import Header from '../../component/Header/header';
 import Home from '../../component/Home/home';
@@ -16,10 +15,7 @@ class Elearn extends Component {
   )
   return (
    <Fragment>
-    <Header
-     src={this.props.loggedIn ? this.props.userData.imageUrl : null}
-     fullname={this.props.loggedIn ? this.props.userData.name : null}
-     loggedIn={this.props.loggedIn} />
+    <Header />
     {route}
     <Footer />
    </Fragment >
@@ -27,12 +23,4 @@ class Elearn extends Component {
  }
 }
 
-const mapStateToProps = (state) => {
- return {
-  loggedIn: state.authenticate.loggedIn,
-  userData: state.authenticate.userData,
-  id: state.playListId
- }
-};
-
-export default connect(mapStateToProps, null)(Elearn);
+export default Elearn;
