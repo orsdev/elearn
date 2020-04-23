d2205c3f767e9496e0af3b26de2e5014559b616c
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-23 18:10:35 +0100

    studentAuthentication and intructorAuthentication added to combineReducer

--- src/store/reducers/combinedReducer.js
+++ src/store/reducers/combinedReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
-import authenticate from './authentication/authenticate';
+import studentAuthentication from './authentication/studentAuthentication';
+import instructorAuthentication from './authentication/instructorAuthentication';
 import playListId from './playListId/playListId';
 import playListItems from './playListItems/playListItems';
 import users from './users/users';
 
 const combinedReducer = combineReducers({
- authenticate,
+ studentAuthentication,
+ instructorAuthentication,
  playListId,
  playListItems,
  users
