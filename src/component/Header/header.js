dea0dbd7c1d973b9ec9d8bdc3603a22ffbcdc0a2
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-23 18:03:52 +0100

    unused imported file removed from header.js

--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import LogoutButton from '../Button/logoutButton/logoutButton';
 
 const Header = (props) => {
