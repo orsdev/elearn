977238ffe973b04bb27dd5410a10debc93f8d0ca
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-27 20:45:52 +0100

    hero component and test refactored

--- src/component/Hero/hero.js
+++ src/component/Hero/hero.js
@@ -1,8 +1,40 @@
-import React from 'react';
-import StudentLogin from '../Button/loginButton/studentLogin';
-import InstructorLogin from '../Button/loginButton/instructorLogin';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import * as action from '../../store/action';
+import jsonServer from '../../api/jsonServer';
+import GoogleLoginButton from '../Button/loginButton/googleLoginButton';
 
 const Hero = (props) => {
+
+ useEffect(() => {
+ }, [props.auth, props.users.success, props.authData]);
+
+ //Google auth success function
+ const responseGoogle = (response) => {
+  if (response && !response.error) {
+   props.onAuthenticate(response.profileObj, true);
+
+   if (response.profileObj) {
+    const { email } = response.profileObj;
+    //make a httpRequest to student api
+    props.onGetUser('students?id=' + email);
+   }
+  }
+ };
+
+ //save new users details in json-server
+ if (props.users.success && props.users.user.length === 0) {
+
+  const data = {
+   id: props.authData.email,
+   name: props.authData.name,
+   starred: []
+  };
+  //save user data to json-server database
+  jsonServer.post('/students', data);
+ }
+
 
  return (
   <div
@@ -13,16 +45,42 @@ const Hero = (props) => {
     data-test="hero-body">
     <h1>HONE YOUR SKILLS</h1>
     <p>
-     Learn from professional intructors. It’s easy to learn a new skill, and even easier to get started.
+     Learn from professional intructors.
+     It’s easy to learn a new skill,
+     and even easier to get started.
      </p>
     <div className="button-wrapper">
-     <StudentLogin />
-     <InstructorLogin />
+     <GoogleLoginButton
+      text="Student Login"
+      responseGoogle={responseGoogle}
+     />
     </div>
    </div>
-
   </div >
  )
 };
 
-export default Hero;
+const mapStateToProps = (state) => {
+ return {
+  auth: state.authentication.auth,
+  authData: state.authentication.authData,
+  users: state.users
+ }
+};
+
+const mapDispatchToProps = (dispatch) => {
+ return {
+  onAuthenticate: (authData, auth) => dispatch(action.authenticate(authData, auth)),
+  onGetUser: (query) => dispatch(action.getUsers(query))
+ }
+}
+
+Hero.propTypes = {
+ auth: PropTypes.bool,
+ authData: PropTypes.object,
+ users: PropTypes.object,
+ onAuthenticate: PropTypes.func,
+ onGetUser: PropTypes.func
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Hero);
--- src/component/Hero/hero.test.js
+++ src/component/Hero/hero.test.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import Hero from './hero';
 import { shallow } from 'enzyme';
-import { findByAttr } from '../../test/utils'
+import { findByAttr, checkProps, storeFactory } from '../../test/utils'
 
 const setUp = () => {
- const component = shallow(<Hero />);
+ const store = storeFactory(undefined);
+ const component = shallow(<Hero store={store} />).dive().dive();
  return component;
 };
 
 describe('Hero Component', () => {
 
- let wrapper;
- beforeEach(() => {
-  wrapper = setUp();
- });
-
  test('Should render with error', () => {
+  let wrapper = setUp();
   const component = findByAttr(wrapper, 'hero-component');
   expect(component.length).toBe(1);
  });
 
  test('Should render with hero-body', () => {
+  let wrapper = setUp();
   const component = findByAttr(wrapper, 'hero-body');
   expect(component.length).toBe(1);
+ });
+
+
+ test('does not throw warning with expected props', () => {
+  const expectedProps = {
+   auth: false,
+   responseGoogle: () => { },
+   authData: {},
+   users: {},
+   onAuthenticate: () => { },
+   onGetUser: () => { }
+  };
+
+  checkProps(Hero, expectedProps);
  });
 
 })
