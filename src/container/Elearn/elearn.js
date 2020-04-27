3423f644fdee4a9771da51429de9039e24702f05
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-27 21:38:23 +0100

    more routes added to elearn component

--- src/container/Elearn/elearn.js
+++ src/container/Elearn/elearn.js
@@ -2,7 +2,11 @@ import React, { Component, Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from '../../component/Header/header';
 import Home from '../../component/Home/home';
+import Dashboard from '../../component/Dashboard/dashboard';
 import Footer from '../../component/Footer/footer';
+import InstructorPage from '../../component/InstructorPage/instructorPage';
+
+
 
 class Elearn extends Component {
 
@@ -11,6 +15,8 @@ class Elearn extends Component {
   let route = (
    <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/dashboard" component={Dashboard} />
+    <Route exact path="/instructor" component={InstructorPage} />
    </Switch>
   )
   return (
