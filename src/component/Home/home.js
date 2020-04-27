b6df2cbef30f5b40733acca9eab65a2bbeb69e5a
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-27 21:16:24 +0100

    home component modified

--- src/component/Home/home.js
+++ src/component/Home/home.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Hero from '../Hero/hero';
 import HomeCourses from '../HomeCourses/homeCourses';
+import Banner from '../Banner/banner';
 
 const Home = (props) => {
 
@@ -8,6 +9,7 @@ const Home = (props) => {
   <Fragment>
    <Hero />
    <HomeCourses />
+   <Banner />
   </Fragment>
  )
 };
