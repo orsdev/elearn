a10885ab78d2c6891337663f36a01aa298d582f8
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-28 15:02:38 +0100

    user.test.js added to app

---
+++ src/store/reducers/users/user.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { types } from '../../types';
+import Users from './users';
+
+describe('User Reducer', () => {
+ test('Should return default state', () => {
+  const user = Users(null, {});
+  expect(user).toEqual(null);
+ });
+
+ test('Should return new state if action.type(USERS_DATA) is provided', () => {
+
+  const state = {
+   user: [],
+   success: true
+  }
+
+  const action = {
+   type: types.USERS_DATA,
+   data: state.user,
+   success: state.success
+  }
+
+  const user = Users(state, action);
+  expect(user).toEqual(state);
+ });
+
+});
