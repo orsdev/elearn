9bf9ff3654a35c431b5cad093d5f6ad3b8ea81d4
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-28 23:18:20 +0100

    allCourses reducer added to redux

--- src/store/reducers/combinedReducer.js
+++ src/store/reducers/combinedReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import allCourses from './allCourses/allCourse';
 import authentication from './authentication/authentication';
 import users from './users/users';
 
 const combinedReducer = combineReducers({
  authentication,
+ allCourses,
  users
 });
 
