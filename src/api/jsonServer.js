4319a9eb281126961880e1274830d3cc8d8bb281
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-04-29 11:47:33 +0100

    jsonServer.js modified

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
- baseURL: "http://localhost:5000",
+ baseURL: process.env.REACT_APP_DATABASE_API,
  headers: { "Content-Type": "application/json" }
 });
