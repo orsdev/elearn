119bac8c2b12538e3714a2a069c3653cf11f1243
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-05-02 11:51:06 +0100

    allow origin header added to http request

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -4,6 +4,7 @@ export default axios.create({
  baseURL: process.env.REACT_APP_DATABASE_API,
  headers: {
   "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "https://elearn-udemy.herokuapp.com"
+  "Access-Control-Allow-Origin": "https://elearn-udemy.herokuapp.com",
+   "Access-Control-Allow-Crededentials": true
  }
 });
