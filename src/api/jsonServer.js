70f674fe913298e6a44d88307d05b0b6b3eab13a
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-05-02 11:28:20 +0100

    jsonServer file modified

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
  baseURL: process.env.REACT_APP_DATABASE_API,
- headers: { "Content-Type": "application/json" }
+ headers: {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "https://elearn-udemy.herokuapp.com"
+ }
 });
