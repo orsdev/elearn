9fbd3a4113a2fb0fada9618ef1818baa6adf0727
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-05-02 16:28:13 +0100

    axios instance modified

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 export default axios.create({
  baseURL: process.env.REACT_APP_DATABASE_API,
  headers: {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "https://elearn-udemy.herokuapp.com",
-   "Access-Control-Allow-Crededentials": true
+  "Content-Type": "application/json"
  }
 });
