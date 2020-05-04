e068f4fa82f74b80f937231d21699d2e494ba9ff
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-05-04 12:43:46 +0100

    instructorPage converted to functional component & latest course not showing bug fixed

--- src/component/InstructorPage/instructorPage.js
+++ src/component/InstructorPage/instructorPage.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
 import * as action from '../../store/action';
 import PropTypes from 'prop-types';
@@ -8,132 +8,135 @@ import { Link } from 'react-router-dom';
 import Notification from '../Notification/notification';
 import Spinner from '../Spinner/spinner';
 
-class InstructorPage extends Component {
+const InstructorPage = (props) => {
 
- componentDidUpdate(prevProps, prevState) {
+ useEffect(() => {
 
-  if (this.props.auth) {
-   if (this.props.authData !== prevProps.authData) {
-    const { email } = this.props.authData;
-    this.props.onGetUser('tutors?id=' + email);
-   }
+  /*
+   when user is authenticated
+   get user details from json-server(db)
+   */
+  if (props.auth && props.authData) {
+   const { email } = props.authData;
+   props.onGetUser('tutors?id=' + email);
   }
 
-  if (this.props.auth) {
-   if (this.props.users.success !== prevProps.users.success) {
-    if (this.props.users.user.length && this.props.users.user[0].courses.length) {
-     //call function and update redux state
-     this.props.getUserCourses(this.props.users.user[0].courses, false);
+ }, [props.auth]);
 
-    } else {
-     //call function and update redux state
-     this.props.getUserCourses(null, true);
-    }
-   }
-  }
+ useEffect(() => {
 
   //save new users details in json-server
-  if (this.props.auth) {
-   if (this.props.users.success !== prevProps.users.success
-    && this.props.users.user.length === 0) {
+  if (props.users.success
+   && props.users.user.length === 0) {
 
-    const data = {
-     id: this.props.authData.email,
-     name: this.props.authData.name,
-     courses: []
-    };
+   const data = {
+    id: props.authData.email,
+    name: props.authData.name,
+    courses: []
+   };
 
-    //save user data to json-server database
-    jsonServer.post('/tutors', data);
+   //save user data to json-server database
+   jsonServer.post('/tutors', data);
+  }
+
+ }, [props.users.success]);
+
+ useEffect(() => {
+
+  if (props.auth) {
+   if (props.users.user.length
+    && props.users.user[0].courses.length) {
+    //call function and update redux state
+    props.getUserCourses(props.users.user[0].courses, false);
+
+   } else {
+    //call function and update redux state
+    props.getUserCourses(null, true);
    }
   }
 
- }
+ }, [props.users.user]);
 
  //Google auth success function
- responseGoogle = (response) => {
+ const responseGoogle = (response) => {
   if (response && !response.error) {
-   this.props.onAuthenticate(response.profileObj, true);
+   props.onAuthenticate(response.profileObj, true);
 
    if (response.profileObj) {
     const { email } = response.profileObj;
     //make a httpRequest to tutors api
-    this.props.onGetUser('tutors?id=' + email);
+    props.onGetUser('tutors?id=' + email);
    }
   }
  };
 
- render() {
-
-  let course;
-  if (this.props.userCourses.courses) {
-   course = this.props.userCourses.courses.map((value, index) => {
-    return (
-     <Fragment key={index}>
-      <div className="grid-item" id={value.url}>
-       <video width="400" controls>
-        <source src={value.url} type="video/mp4" />
+ if (props.userCourses.courses) {
+  var course = props.userCourses.courses.map((value, index) => {
+   return (
+    <Fragment key={index}>
+     <div className="grid-item" id={value.url}>
+      <video width="400" controls>
+       <source src={value.url} type="video/mp4" />
       Your browser does not support HTML5 video.
      </video>
-       <div className="course-body">
-        <h4 className="course-title">{value.title}</h4>
-        <p className="course-about">{value.description}</p>
-        <p className="course-author author-name">{value.author}</p>
-       </div>
-      </div>
-     </Fragment>
-    )
-   })
-  }
-
-  return (
-   <div
-    data-test="instructorPage-component"
-    className="instructorPage">
-    <div className="instructorPage-hero">
-     <div className="instructorPage-hero-body">
-      <h1>Launch Your Course For Free</h1>
-      <div className="button-wrapper">
-       <GoogleLoginButton
-        text="Instructor Login"
-        responseGoogle={this.responseGoogle} />
-       {
-        this.props.auth ?
-         <Link to="/dashboard"> My Dashboard </Link>
-         :
-         null
-       }
+      <div className="course-body">
+       <h4 className="course-title">{value.title}</h4>
+       <p className="course-about">{value.description}</p>
+       <p className="course-author author-name">{value.author}</p>
       </div>
      </div>
-    </div>
-    <div className="instructorPage-main">
-     <h2>My Courses</h2>
-     {!this.props.auth ?
-      <Notification
-       text="Login to see your courses."
-      /> :
-      <div className="grid-container">
-       {course}
-      </div>
-     }
-     {this.props.auth
-      && this.props.userCourses.isEmpty ?
-      <Notification
-       text="No course was found."
-      /> :
-      null
-     }
-     {this.props.auth
-      && !this.props.userCourses.courses
-      && !this.props.userCourses.isEmpty
-      ?
-      <Spinner />
-      : null
-     }
+    </Fragment>
+   )
+  })
+ }
+
+ return (
+  <div
+   data-test="instructorPage-component"
+   className="instructorPage">
+   <div className="instructorPage-hero">
+    <div className="instructorPage-hero-body">
+     <h1>Launch Your Course For Free</h1>
+     <div className="button-wrapper">
+      <GoogleLoginButton
+       text="Instructor Login"
+       responseGoogle={responseGoogle} />
+      {
+       props.auth ?
+        <Link to="/dashboard"> My Dashboard </Link>
+        :
+        null
+      }
+     </div>
     </div>
    </div>
-  );
- }
+   <div className="instructorPage-main">
+    <h2>My Courses</h2>
+    {!props.auth ?
+     <Notification
+      text="Login to see your courses."
+     /> :
+     <div className="grid-container">
+      {course}
+     </div>
+    }
+    {props.auth
+     && props.userCourses.isEmpty ?
+     <Notification
+      text="No course was found."
+     /> :
+     null
+    }
+    {props.auth
+     && !props.userCourses.courses
+     && !props.userCourses.isEmpty
+     ?
+     <Spinner />
+     : null
+    }
+   </div>
+  </div>
+ );
 }
 
 const mapStateToProps = (state) => {
